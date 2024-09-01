fa9342f4ad66dbde3a72e9faba1daa0272366228
JaxtonGray <jaxtongray@gmail.com>
2024-09-01 17:28:43 -0600

    Changes to HydroGFD download

---
+++ Data/HydroGFD/tempDownload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=def-stadnykt-ab
+#SBATCH --job-name=Precip_Download
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000MB
+#SBATCH --time=1:00:00
+#SBATCH --mail-user=jaxton.gray@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Setup the environment
+module load python/3.11
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install 'cdsapi>=0.7.0'
+
+# Run the program
+python downloadTemp.py
