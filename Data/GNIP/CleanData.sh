a1a3765e6db691d2c8de92782831cf681d22557d
JaxtonGray <jaxtongray@gmail.com>
2024-09-02 16:59:24 -0600

    Add bash script

---
+++ Data/GNIP/CleanData.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=def-stadnykt-ab
+#SBATCH --job-name=Clean_GNIP_Data
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000MB
+#SBATCH --time=1:00:00
+#SBATCH --mail-user=jaxton.gray@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Setup the environment
+module load python/3.11
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
+pip install --no-index -r req_clean.txt
+
+python DataClean_Combine.py
