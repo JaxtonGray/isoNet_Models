c255189d73319435d55000340a41c6b1a7d64d2e
JaxtonGray <jaxtongray@gmail.com>
2024-09-30 20:41:53 -0600

    Add missing required files

---
+++ Models/Model_4/training.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-stadnykt-ab
+#SBATCH --job-name=model_training
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000MB
+#SBATCH --time=2:00:00
+#SBATCH --mail-user=jaxton.gray@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Set up the environment
+module load python/3.11.5
+module load proj
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+pip install --no-index tensorflow
+
+# Run the training script
+python modelTraining.py
+
+
