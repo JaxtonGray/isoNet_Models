4b394c26afebdcfb62c0544424859e1c54d89828
Jaxton Gray <=>
2024-10-07 16:54:49 -0400

    Added new training script

---
+++ Models/Model_1/traintune.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=def-stadnykt-ab
+#SBATCH --job-name=model_training
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000MB
+#SBATCH --time=4:00:00
+#SBATCH --mail-user=jaxton.gray@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load python/3.11.5
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+python modelTraining.py
+
+
