0128aabeab94cc8421d747e071477f0ec47a9926
Jaxton Gray <jaxtongray@gmail.com>
2024-10-20 21:38:40 -0600

    Update trainall script

--- trainAll.sh
+++ trainAll.sh
@@ -17,6 +17,11 @@ echo $modelName
 
 cd Models/$modelName
 
+# Extract the information from Model_Info.txt
+modelNum=$(sed -n 1p Model_Info.txt)
+modelScheme=$(sed -n 2p Model_Info.txt)
+modelFeatures=$(sed -n 3p Model_Info.txt)
+
 # Set up the environment
 module load python/3.11.5
 module load proj
@@ -28,4 +33,4 @@ pip install --no-index -r requirements.txt
 pip install --no-index tensorflow
 
 # Run the training script
-python modelTraining.py
+python ../modelTraining.py $modelNum $modelScheme $modelFeatures
