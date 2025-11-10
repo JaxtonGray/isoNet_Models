9a50d467eebbeab03c4f1633cd67ab1f98112a5a
Jaxton Gray <123155976+JaxtonGray@users.noreply.github.com>
2025-11-10 16:38:00 -0700

    update train multiple

--- trainMultiple.sh
+++ trainMultiple.sh
@@ -15,13 +15,6 @@
 modelName=$(sed -n ${SLURM_ARRAY_TASK_ID}p modelList.txt)
 echo $modelName
 
-cd Models/$modelName
-
-# Extract the information from Model_Info.txt
-modelNum=$(sed -n 1p Model_Info.txt)
-modelScheme=$(sed -n 2p Model_Info.txt)
-modelFeatures=$(sed -n 3p Model_Info.txt)
-
 # Set up the environment
 module load python/3.11.5
 module load proj
@@ -33,4 +26,4 @@ pip install --no-index -r requirements.txt
 pip install --no-index tensorflow
 
 # Run the training script
-python ../modelTraining.py "$modelNum" "$modelScheme" "$modelFeatures"
+python Model_Training/modelTraining.py  "$modelName"
