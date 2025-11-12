c558edd026dc6843c72fddb00eb73c3847ff1973
Jaxton Gray <jaxtongray@gmail.com>
2025-11-12 14:03:33 -0700

    Add a file to test a model

---
+++ test.py
@@ -0,0 +1,32 @@
+# Import libraries
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+
+# Load dataset
+data = pd.read_csv(r'Data/DataTest.csv')
+
+# Preprocess data
+data['Year'] = pd.to_datetime(data['Date']).dt.year
+data['JulianDay'] = pd.to_datetime(data['Date']).dt.dayofyear
+data['JulianDay_Sin'] = np.sin(2 * np.pi * data['JulianDay'] / 365)
+
+# Set the input features apart from the target variable
+features = data[['Lat', 'Lon', 'Alt', 'Year', 'JulianDay_Sin']]
+
+# Load pre-trained model
+model = keras.models.load_model(r'Models/Global_B/Model_Run1/Model_Run1.keras')
+
+# Make predictions
+predictions = model.predict(features)
+
+# Save predictions to CSV
+output = pd.DataFrame({
+    'Date': data['Date'],
+    'Lat': data['Lat'],
+    'Lon': data['Lon'],
+    'Alt': data['Alt'],
+    'Predicted_Value': predictions.flatten()
+})
+output.to_csv(r'Predictions.csv', index=False)
---
+++ testRun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=def-stadnykt-ab
+#SBATCH --job-name=model_training
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000MB
+#SBATCH --time=00:00:10
+#SBATCH --mail-user=jaxton.gray@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# !/bin/bash
+# Set up the environment
+module load python/3.11
+module load proj
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index tensorflow pandas numpy
+
+# Run the training script
+python test.py
