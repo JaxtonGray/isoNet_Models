bf332b83e26aac00d837bc02835f02f62029de9d
Jaxton Gray <jaxtongray@gmail.com>
2025-11-12 14:53:36 -0700

    Update to test all models

--- testRun.sh
+++ testRun.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64000MB
-#SBATCH --time=00:05:00
+#SBATCH --time=00:20:00
 #SBATCH --mail-user=jaxton.gray@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
