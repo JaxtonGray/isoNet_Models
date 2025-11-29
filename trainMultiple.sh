98d8f3cef7d05d65b8963e16bc038c4241488bfd
Jaxton Gray <=>
2025-11-28 21:45:26 -0500

    Fix code to train more than just first line

--- trainMultiple.sh
+++ trainMultiple.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=2:30:00
 #SBATCH --mail-user=jaxton.gray@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --array=1
+#SBATCH --array=1-30
 #SBATCH --output=SLURM_Output/model_training_%j_%a.out
 
 # !/bin/bash
