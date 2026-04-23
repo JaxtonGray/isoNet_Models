fd551f1458d242733e1e4add36597c8b2abe0484
Jaxton Gray <=>
2026-04-23 17:11:54 -0400

    All model runs

--- trainMultiple.sh
+++ trainMultiple.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=03:30:00
 #SBATCH --mail-user=jaxton.gray@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --array=1-10
+#SBATCH --array=1-44
 #SBATCH --output=SLURM_Output/model_training_%A_%a.out
 
 # !/bin/bash
